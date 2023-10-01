6b56a6b5dececdba122c61c20f5fa1b196f8d8dc
Billal-07 <bilhashmi@icloud.com>
2023-09-30 19:31:28 -0700

    Cards Component Created

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,11 +1,17 @@
 import SignUp from './Admin/SignUp';
 import './App.css';
+import Cards from './Components/Cards';
+import Carousel from './Components/Carousel';
+import HomePage from './Pages/HomePage';
 import User_SignUp from './User/User_SignUp';
 import AddProduct from './products/AddProducts';
 
 function App() {
   return (
-    <div className='h-screen'>      
+    <div>
+      <Carousel />
+      {/* <Cards /> */}
+      {/* <HomePage /> */}
       {/* <SignUp /> */}
       {/* <User_SignUp /> */}
       {/* <AddProduct />   */}
---
+++ FrontEnd/src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cards from '../Components/Cards'
+
+const HomePage = () => {
+    return (
+        <div className='flex justify-center h-fit max-w-full '> 
+            <Cards />
+        </div>
+    ) 
+}
+
+export default HomePage
